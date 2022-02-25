dfe92bbc4c1dd9bb587482b8cb9ff409d2f642a7
omkarlondhe <londheomkar10@gmail.com>
2022-02-25 20:24:46 +0530

    first commit

---
+++ src/main/java/com/example/webdocker/WebdockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdockerApplication.class, args);
+	}
+
+}
